acaf0c69e7d8b3fba6ffd27e6bf13044791649e1
Lucky <emily.ct.adams@gmail.com>
2026-06-29 16:38:07 +0100

    struggling to animate tickets in script

--- burger-builder/Assets/Scripts/Managers/TicketUIManager.cs
+++ burger-builder/Assets/Scripts/Managers/TicketUIManager.cs
@@ -1,13 +1,23 @@
-using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 public class TicketUIManager : MonoBehaviour
 {
+    [Header ("Objects")]
     [SerializeField] private GameObject m_ticketPrefab;
     [SerializeField] private Transform m_ticketParent;
+    [SerializeField] private Transform m_ticketSpawn;
+    [SerializeField] private RectTransform m_ticketParentRect;
+    [SerializeField] private Canvas m_canvas;
+
+    [Header("Values")]
+    [SerializeField] private float m_slideDuration = 5f;
+    [SerializeField] private float m_padding = 5.0f;
 
     private List<GameObject> m_tickets = new List<GameObject>();
-    private Canvas m_canvas;
+
+    private float m_targetDown, m_targetLeft;
 
     private void Awake()
     {
@@ -16,19 +26,20 @@ public class TicketUIManager : MonoBehaviour
         if (m_ticketParent == null)
             Debug.LogError("Ticket parent is null");
 
-        m_canvas = FindFirstObjectByType<Canvas>();
+        m_targetLeft = m_ticketParentRect.anchoredPosition.x - (m_ticketParentRect.rect.width * 0.5f);
     }
 
     //spawn ticket UI and populate the UI using the data
     public void SpawnTicket(TicketData data)
     {
         //spawn it in canvas so it isnt locked into the parent's group layout immediately
-        GameObject newTicket = Instantiate(m_ticketPrefab, m_canvas.transform);
+        GameObject newTicket = Instantiate(m_ticketPrefab, m_ticketSpawn);
         TicketUI ui = newTicket.GetComponent<TicketUI>();
         if (ui != null)
         { 
             ui.Populate(data, m_ticketParent);
             m_tickets.Add(newTicket);
+            StartCoroutine(C_AnimateTicket(newTicket, m_targetLeft));
         }
     }
 
@@ -61,7 +72,6 @@ public class TicketUIManager : MonoBehaviour
                 continue;
 
             Destroy(ticket);
-            m_tickets.Remove(ticket);
         }
 
         m_tickets = new List<GameObject>();
@@ -71,5 +81,34 @@ public class TicketUIManager : MonoBehaviour
     {
         Transform[] allChildren = m_ticketParent.GetComponentsInChildren<Transform>();
         return allChildren.Length - 1;
+    }
+
+    private float RecalulcateTarget(RectTransform ticketRect, float targetX)
+    {
+        float ticketWidth = ticketRect.rect.width;
+
+        int ticketCount = GetTicketCount();
+        return targetX + (ticketCount * ticketWidth);
+    }
+
+    //calc destination/distance using num of present tickets
+    private IEnumerator C_AnimateTicket(GameObject ticket, float targetX)
+    {
+        RectTransform ticketRect = ticket.GetComponent<RectTransform>();
+
+        Vector2 startPos = ticketRect.anchoredPosition;
+        Vector2 targetPos = new Vector2(RecalulcateTarget(ticketRect, targetX), startPos.y);
+        float elapsedTime = 0f;
+
+        //lerp x position over time
+        while (elapsedTime < m_slideDuration)
+        {
+            elapsedTime += Time.unscaledDeltaTime;
+            ticketRect.anchoredPosition = Vector3.Lerp(startPos, targetPos, elapsedTime / m_slideDuration);
+            yield return null;
+        }
+
+        ticketRect.anchoredPosition = targetPos;
+        ticket.transform.SetParent(m_ticketParent);
     }
 }
