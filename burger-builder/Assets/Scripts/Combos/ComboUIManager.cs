24e8c1951d1b0b77ce7f3b59bc0f294467895dc4
Lucky <emily.ct.adams@gmail.com>
2026-07-24 01:36:23 +0100

    updated combo bars

--- burger-builder/Assets/Scripts/Combos/ComboUIManager.cs
+++ burger-builder/Assets/Scripts/Combos/ComboUIManager.cs
@@ -1,72 +1,24 @@
 using System;
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections.Generic;
-
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 
 public class ComboUIManager : MonoBehaviour
 {
     [SerializeField] private Image m_barImage;
-    
-    [Header("Bar Set Up")]
-    [SerializeField] private List<float> m_barFillSegments = new List<float>();
-    [SerializeField] private List<bool> m_updateFillAmounts = new List<bool>();
-    [SerializeField] private bool m_resetSegments = false;
 
     private void Awake()
     {
         if (m_barImage == null)
             Debug.LogError("No Bar Image!");
+        else
+        {
+            m_barImage.fillAmount = 0.05f;
+        }
     }
 
     public void UpdateCombo(float combo)
     {
-        //m_comboText.text = $"{combo}";
+        Debug.Log(combo);
+        m_barImage.fillAmount = combo / 10;
     }
-    
-#if UNITY_EDITOR
-
-    private void OnValidate()
-    {
-        UpdateButtons();
-        UpdateFillSegments();
-        ResetSegments();
-    }
-
-    private void UpdateButtons()
-    {
-        if (m_barFillSegments.Count != m_updateFillAmounts.Count)
-            m_updateFillAmounts = new List<bool>(new bool [m_barFillSegments.Count]);
-    }
-
-    private void UpdateFillSegments()
-    {
-        for (int i = 0; i < m_updateFillAmounts.Count; i++)
-        {
-            if (m_updateFillAmounts[i] == true)
-            {
-                if (m_barImage != null && m_barImage.type == Image.Type.Filled)
-                    m_barFillSegments[i] = m_barImage.fillAmount;
-
-                m_updateFillAmounts[i] = false;
-            }
-        }
-    }
-
-    private void ResetSegments()
-    {
-        if (m_resetSegments)
-        {
-            for (int i = 0; i < m_barFillSegments.Count; i++)
-            {
-                m_barFillSegments[i] = 0;
-            }
-            m_resetSegments = false;
-        }
-    }
-
-#endif
 }
